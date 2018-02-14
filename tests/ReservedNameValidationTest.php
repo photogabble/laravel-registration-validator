8e86252ad78877eae29e22f514166d4f14606e5b
Simon Dann <carbontwelve@users.noreply.github.com>
2018-02-14 19:08:14 +0000

    Adding more assertions to test

--- tests/ReservedNameValidationTest.php
+++ tests/ReservedNameValidationTest.php
@@ -3,13 +3,14 @@ namespace Photogabble\LaravelRegistrationValidator\Tests;
 
 class ReservedNameValidationTest extends ValidationBase
 {
-
-    /**
-     * @expectedException \Illuminate\Validation\ValidationException
-     */
     public function testReservedNameValidator()
     {
-        $this->validator->validate(['username' => 'carbontwelve@users.noreply.github.com'], ['username' => 'not-reserved-name']);
-    }
+        $this->assertFalse($this->validator->make(['username' => 'carbontwelve@users.noreply.github.com'], ['username' => 'not-reserved-name'])->passes());
+        $this->assertFalse($this->validator->make(['username' => 'webmaster'], ['username' => 'not-reserved-name'])->passes());
 
+        $this->assertTrue($this->validator->make(['username' => 'photogabble'], ['username' => 'not-reserved-name'])->passes());
+        $this->assertTrue($this->validator->make(['username' => 'hello-world'], ['username' => 'not-reserved-name'])->passes());
+        $this->assertTrue($this->validator->make(['username' => 'hello.world'], ['username' => 'not-reserved-name'])->passes());
+
+    }
 }
